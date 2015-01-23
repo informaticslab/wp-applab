bada9ea1f74f94ac6c1fddf6efef03aa1d3025dd
Greg Ledbetter <gsledbetter@gmail.com>
2015-01-23 13:43:02 -0500

    Create script to update files on server.

---
+++ scripts/update-wordpress.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# app-release plug-in files
+cp ~/informaticslab/wp-applab/wp-content/plugins/app-release/app-release.php /var/www/html/wordpress/wp-content/plugins/app-release
+cp ~/informaticslab/wp-applab/wp-content/plugins/app-release/release-mgr.php /var/www/html/wordpress/wp-content/plugins/app-release
+
+# template files for DesignWall Focus themes
+cp ~/informaticslab/wp-applab/wp-content/themes/dw-focus/single-app_release.php /var/www/html/wordpress/wp-content/themes/dw-focus
+cp ~/informaticslab/wp-applab/wp-content/themes/dw-focus/archive-app_release.php /var/www/html/wordpress/wp-content/themes/dw-focus
