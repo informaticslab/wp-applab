fc1f016d9d64136e3952d6f12bac13572fbcb850
Greg Ledbetter <gsledbetter@gmail.com>
2015-01-26 21:20:26 -0500

    Change project identifiers in release-mgr. Remove debug loggin from templates. Add path variables to shell script.

--- scripts/update-wordpress.sh
+++ scripts/update-wordpress.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
 
+#dest='/var/www/html'
+dest='/Users/jtq6/vagrants/vagrantpress'
+
+#temp='dw-focus'
+temp='twentytwelve'
+
+
 # app-release plug-in files
-cp ~/informaticslab/wp-applab/wp-content/plugins/app-release/app-release.php /var/www/html/wordpress/wp-content/plugins/app-release
-cp ~/informaticslab/wp-applab/wp-content/plugins/app-release/release-mgr.php /var/www/html/wordpress/wp-content/plugins/app-release
+cp ~/informaticslab/wp-applab/wp-content/plugins/app-release/app-release.php $dest/wordpress/wp-content/plugins/app-release
+cp ~/informaticslab/wp-applab/wp-content/plugins/app-release/release-mgr.php $dest/wordpress/wp-content/plugins/app-release
 
 # template files for DesignWall Focus themes
-cp ~/informaticslab/wp-applab/wp-content/themes/dw-focus/single-app_release.php /var/www/html/wordpress/wp-content/themes/dw-focus
-cp ~/informaticslab/wp-applab/wp-content/themes/dw-focus/archive-app_release.php /var/www/html/wordpress/wp-content/themes/dw-focus
-cp ~/informaticslab/wp-applab/wp-content/themes/dw-focus/index.php /var/www/html/wordpress/wp-content/themes/dw-focus
-cp ~/informaticslab/wp-applab/wp-content/themes/dw-focus/style.css /var/www/html/wordpress/wp-content/themes/dw-focus
+cp ~/informaticslab/wp-applab/wp-content/themes/$temp/single-app_release.php $dest/wordpress/wp-content/themes/$temp
+cp ~/informaticslab/wp-applab/wp-content/themes/$temp/archive-app_release.php $dest/wordpress/wp-content/themes/$temp
+cp ~/informaticslab/wp-applab/wp-content/themes/$temp/index.php $dest/wordpress/wp-content/themes/$temp
+cp ~/informaticslab/wp-applab/wp-content/themes/$temp/style.css $dest/wordpress/wp-content/themes/$temp
 
