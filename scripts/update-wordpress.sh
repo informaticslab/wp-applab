784fe342fe8496afee730db6f642ac8dc79f9655
Greg Ledbetter <gsledbetter@gmail.com>
2015-01-28 11:02:25 -0500

    Updated script to include destination on WP4 server.

--- scripts/update-wordpress.sh
+++ scripts/update-wordpress.sh
@@ -1,21 +1,27 @@
 #!/bin/sh
 
+# WP4 VM location
 dest='/var/www/html'
-#dest='/Users/jtq6/vagrants/vagrantpress'
+
+# WP3 VM location
+#dest='/var/www/html/wordpress'
+
+# Greg's wordpress test vagrant
+#dest='/Users/jtq6/vagrants/vagrantpress/wordpress'
 
 temp='dw-focus'
 #temp='twentytwelve'
 
 
 # app-release plug-in files
-cp ~/informaticslab/wp-applab/wp-content/plugins/app-release/app-release.php $dest/wordpress/wp-content/plugins/app-release
-cp ~/informaticslab/wp-applab/wp-content/plugins/app-release/release-mgr.php $dest/wordpress/wp-content/plugins/app-release
-cp ~/informaticslab/wp-applab/wp-content/plugins/app-release/mobile_apps_wordpress.php $dest/wordpress/wp-content/plugins/app-release
-cp ~/informaticslab/wp-applab/wp-content/plugins/app-release/gen_manifests.php $dest/wordpress/wp-content/plugins/app-release
+cp ~/informaticslab/wp-applab/wp-content/plugins/app-release/app-release.php $dest/wp-content/plugins/app-release
+cp ~/informaticslab/wp-applab/wp-content/plugins/app-release/release-mgr.php $dest/wp-content/plugins/app-release
+cp ~/informaticslab/wp-applab/wp-content/plugins/app-release/mobile_apps_wordpress.php $dest/wp-content/plugins/app-release
+cp ~/informaticslab/wp-applab/wp-content/plugins/app-release/gen_manifests.php $dest/wp-content/plugins/app-release
 
 
 # template files for DesignWall Focus themes
-cp ~/informaticslab/wp-applab/wp-content/themes/$temp/single-app_release.php $dest/wordpress/wp-content/themes/$temp
-cp ~/informaticslab/wp-applab/wp-content/themes/$temp/archive-app_release.php $dest/wordpress/wp-content/themes/$temp
-cp ~/informaticslab/wp-applab/wp-content/themes/$temp/index.php $dest/wordpress/wp-content/themes/$temp
-cp ~/informaticslab/wp-applab/wp-content/themes/$temp/style.css $dest/wordpress/wp-content/themes/$temp
+cp ~/informaticslab/wp-applab/wp-content/themes/$temp/single-app_release.php $dest/wp-content/themes/$temp
+cp ~/informaticslab/wp-applab/wp-content/themes/$temp/archive-app_release.php $dest/wp-content/themes/$temp
+cp ~/informaticslab/wp-applab/wp-content/themes/$temp/index.php $dest/wp-content/themes/$temp
+cp ~/informaticslab/wp-applab/wp-content/themes/$temp/style.css $dest/wp-content/themes/$temp
