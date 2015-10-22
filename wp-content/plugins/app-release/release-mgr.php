b38559eb7194785a8fc1ff9b5aa3bd71b3495d1e
Greg Ledbetter <gsledbetter@gmail.com>
2015-10-22 14:45:45 -0400

    More tweaks to filenames.

--- wp-content/plugins/app-release/release-mgr.php
+++ wp-content/plugins/app-release/release-mgr.php
@@ -130,7 +130,7 @@ class IosApp extends BaseApp {
         fwrite($manifest_file,  '              <key>kind</key>'."\n");
         fwrite($manifest_file,  '              <string>full-size-image</string>'."\n");
         fwrite($manifest_file,  '              <key>url</key>'."\n");
-        fwrite($manifest_file,  '              <string>https://'.SERVER.$this->ios_dir."/icon512x512.png<</string>\n");
+        fwrite($manifest_file,  '              <string>https://'.SERVER.$this->ios_dir."/icon512x512.png</string>\n");
         fwrite($manifest_file,  '            </dict>'."\n");
         fwrite($manifest_file,  '          </array>'."\n");
         fwrite($manifest_file,  '          <key>metadata</key>'."\n");
